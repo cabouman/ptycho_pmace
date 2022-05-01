470edfcf49f373e788e76d5790fa2400db27a2e4
qiuchenzhai <qzhai@purdue.edu>
2022-05-01 17:26:08 -0400

    Remove image plots from ePIE recon functions

--- ptycho/pie.py
+++ ptycho/pie.py
@@ -1,10 +1,10 @@
-import random
 from ptycho_pmace.utils.utils import *
 from ptycho_pmace.utils.nrmse import *
+import random
 
 
 def epie_recon(init_obj_guess, diffraction_data, projection_coords, obj_ref, probe_ref,
-               num_iter=100, step_sz=1, display_win=None, display=False, save_dir=None):
+               num_iter=100, step_sz=1, cstr_win=None, save_dir=None):
     """
     ePIE reconstruction to perform single estimate on complex transmittance of complex
     object assuming known complex probe function.
@@ -15,19 +15,15 @@ def epie_recon(init_obj_guess, diffraction_data, projection_coords, obj_ref, pro
     :param probe_ref: known or estimated complex probe function.
     :param num_iter: number of iterations.
     :param step_sz: step size parameter for object update function.
-    :param display_win: pre-defined cover/window for comparing reconstruction results.
-    :param display: option to display the reconstruction results.
+    :param cstr_win: pre-defined cover/window for comparing reconstruction results.
     :param save_dir: save reconstruction results to the given directory.
     :return: reconstructed complex transmittance of complex object and error metrics.
     """
-
-    # check directories for saving files
+    # check directories
     if save_dir is not None:
-        obj_fname = save_dir + 'ePIE_img_revy'
         os.makedirs(save_dir, exist_ok=True)
-
-    if display_win is None:
-        display_win = np.ones(init_obj_guess.shape)
+    if cstr_win is None:
+        cstr_win = np.ones_like(init_obj_guess)
 
     # initialization
     num_agts, m, n = diffraction_data.shape
@@ -67,8 +63,8 @@ def epie_recon(init_obj_guess, diffraction_data, projection_coords, obj_ref, pro
         diffr_nrmse_ls.append(diffr_nrmse_val)
 
         # phase normalization and compute the nrmse error between reconstructed image and reference image
-        obj_revy = phase_norm(np.copy(obj_est) * display_win, np.copy(obj_ref) * display_win)
-        obj_nrmse_val = compute_nrmse(obj_revy * display_win, obj_ref * display_win, display_win)
+        obj_revy = phase_norm(np.copy(obj_est) * cstr_win, np.copy(obj_ref) * cstr_win)
+        obj_nrmse_val = compute_nrmse(obj_revy * cstr_win, obj_ref * cstr_win, cstr_win)
         obj_nrmse_ls.append(obj_nrmse_val)
         print('iter =', i, 'img_error =', obj_nrmse_val, 'diffr_err =', diffr_nrmse_val)
 
@@ -76,15 +72,6 @@ def epie_recon(init_obj_guess, diffraction_data, projection_coords, obj_ref, pro
     save_tiff(obj_est, save_dir + 'obj_est_iter_{}.tiff'.format(i + 1))
     save_array(obj_nrmse_ls, save_dir + 'obj_nrmse')
     save_array(diffr_nrmse_ls, save_dir + 'diffr_nrmse')
-
-    # display the reconstructed image and convergence plot
-    plot_cmplx_obj(obj_revy, obj_ref, img_title='ePIE', display_win=display_win, display=display, save_fname=obj_fname)
-    xlabel, ylabel, line_label = 'Number of iterations', 'object NRMSE', r'ePIE ($step size$ = {})'.format(step_sz)
-    plot_nrmse(obj_nrmse_ls, title='Convergence plot of ePIE algorithm', label=[xlabel, ylabel, line_label],
-               step_sz=15, fig_sz=[8, 4.8], display=display, save_fname=save_dir + 'convergence_obj_nrmse')
-    xlabel, ylabel, line_label = 'Number of iterations', 'diffraction NRMSE', r'ePIE ($step size$ = {})'.format(step_sz)
-    plot_nrmse(diffr_nrmse_ls, title='Convergence plot of ePIE algorithm', label=[xlabel, ylabel, line_label],
-               step_sz=15, fig_sz=[8, 4.8], display=display, save_fname=save_dir + 'convergence_diffr_nrmse')
 
     # return the result
     keys = ['obj_revy', 'obj_err', 'diffr_err']
@@ -95,7 +82,7 @@ def epie_recon(init_obj_guess, diffraction_data, projection_coords, obj_ref, pro
 
 
 def epie_joint_recon(init_obj, init_probe, diffraction_data, projection_coords, obj_ref=None, probe_ref=None,
-                     num_iter=100, obj_step_sz=1, probe_step_sz=1, display_win=None, display=False, save_dir=None):
+                     num_iter=100, obj_step_sz=1, probe_step_sz=1, cstr_win=None, save_dir=None):
     """
     Function to perform PIE-type approach for ptychographic image reconstruction.
     :param init_obj: initialized complex object.
@@ -107,23 +94,16 @@ def epie_joint_recon(init_obj, init_probe, diffraction_data, projection_coords, 
     :param num_iter: number of iteration.
     :param obj_step_sz: step size of object update function.
     :param probe_step_sz: step size of probe update function.
-    :param display_win: reconstruction window.
-    :param display: display the reconstruction results.
+    :param cstr_win: reconstruction window.
     :param save_dir: path for saving reconstructed images.
     :return: reconstructed complex image and probe and nrmse.
     """
 
     # check directories for saving files
-    if save_dir is None:
-        obj_fname = None
-        probe_fname = None
-    else:
-        obj_fname = save_dir + 'ePIE_img_revy'
-        probe_fname = save_dir + 'ePIE_probe_revy'
+    if save_dir is not None:
         os.makedirs(save_dir, exist_ok=True)
-
-    if display_win is None:
-        display_win = np.ones(init_obj.shape)
+    if cstr_win is None:
+        cstr_win = np.ones_like(init_obj)
 
     # initialization
     obj_nrmse_ls = []
@@ -160,22 +140,18 @@ def epie_joint_recon(init_obj, init_probe, diffraction_data, projection_coords, 
 
         # phase normalization and scale image to minimize the intensity difference
         if obj_ref is not None:
-            obj_revy = phase_norm(np.copy(obj_est) * display_win, obj_ref * display_win)
+            obj_revy = phase_norm(np.copy(obj_est) * cstr_win, obj_ref * cstr_win)
+            img_nrmse_val = compute_nrmse(obj_revy * cstr_win, obj_ref * cstr_win, cstr_win)
+            obj_nrmse_ls.append(img_nrmse_val)
         else:
             obj_revy = obj_est
-            obj_ref = obj_revy
+
         if probe_ref is not None:
             probe_revy = phase_norm(np.copy(probe_est), probe_ref)
+            probe_nrmse_val = compute_nrmse(probe_revy, probe_ref)
+            probe_nrmse_ls.append(probe_nrmse_val)
         else:
             probe_revy = probe_est
-            probe_ref = probe_revy
-
-        # compute the nrmse error
-        img_nrmse_val = compute_nrmse(obj_revy * display_win, obj_ref * display_win, display_win)
-        obj_nrmse_ls.append(img_nrmse_val)
-        probe_nrmse_val = compute_nrmse(probe_revy, probe_ref, np.ones(probe_revy.shape))
-        probe_nrmse_ls.append(probe_nrmse_val)
-        print('iter =', i, 'img_nrmse =', img_nrmse_val, 'probe_nrmse =', probe_nrmse_val, 'diffr_err =', diffr_nrmse_val)
 
     # calculate time consumption
     elapsed_time = time.time() - start_time
@@ -187,10 +163,6 @@ def epie_joint_recon(init_obj, init_probe, diffraction_data, projection_coords, 
     save_array(obj_nrmse_ls,  save_dir + 'obj_nrmse')
     save_array(probe_nrmse_ls, save_dir + 'probe_nrmse')
     save_array(diffr_nrmse_ls, save_dir + 'diffr_nrmse')
-
-    # display the reconstructed image and probe
-    plot_cmplx_obj(obj_revy, obj_ref, img_title='ePIE', display_win=display_win, display=display, save_fname=obj_fname)
-    plot_cmplx_probe(probe_revy, probe_ref, img_title='ePIE', display=display, save_fname=probe_fname)
 
     # return the result
     keys = ['obj_revy', 'probe_revy', 'obj_err', 'probe_err', 'diffr_err']
