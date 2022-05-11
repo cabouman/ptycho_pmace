61689ac7ba3a9ef45306ff34cb5544afa44c149a
qiuchenzhai <qzhai@purdue.edu>
2022-05-11 11:21:01 -0400

    Merge WF object reconstruction and joint reconstruction functions

--- ptycho/wf.py
+++ ptycho/wf.py
@@ -7,11 +7,11 @@ def wf_obj_func(current_est, probe_ref, diffr_data, coords, discretized_sys_mat,
     Function to determine the gradient descent updates of complex object estimate.
     :param current_est: the current estimate of complex object.
     :param probe_ref: known complex probe.
-    :param diffr_data: phaseless measurements.
+    :param diffr_data: pre-processed diffraction pattern (intensity data).
     :param coords: coordinates of projections.
     :param discretized_sys_mat: to calculate the step size.
     :param param: hyper-parameter, param=1 if Fourier transform is orthonormal.
-    :return: updated esimate of complex object.
+    :return: updated estimate of complex object.
     """
     # Ax = FT{D*P_j*v}
     projected_patch = img2patch(current_est, coords, diffr_data.shape)
@@ -34,7 +34,7 @@ def wf_probe_func(current_est, projected_img, diffr_data, discretized_sys_mat, p
     Function to determine the gradient descent update of complex probe estimate.
     :param current_est: current estimate of complex probe.
     :param projected_img: projected image patch of current estimate of complex object.
-    :param diffr_data: phaseless measurements.
+    :param diffr_data: pre-processed diffraction pattern (intensity data).
     :param discretized_sys_mat: to calculate step size.
     :param param: hypter-param, param=1 if Fourier transform is orthonormal.
     :return: new estimate of complex probe.
@@ -52,123 +52,45 @@ def wf_probe_func(current_est, projected_img, diffr_data, discretized_sys_mat, p
     return output
 
 
-def wf_recon(init_obj_guess, diffract_data, projection_coords, obj_ref, probe_ref,
-             num_iter=100, param=1, accel=True, cstr_win=None, save_dir=None):
+def wf_recon(dp, project_coords, init_obj, init_probe=None, obj_ref=None, probe_ref=None,
+             accel=True, num_iter=100, joint_recon=False, cstr_win=None, save_dir=None):
     """
-    WF/AWF reconstruction to perform single estimate on complex transmittance of complex
-    object assuming known complex probe function.
-    :param init_obj_guess: formulated initial guess of complex transmittance of complex object image.
-    :param diffract_data: pre-processed diffraction patterns (phase-less measurements).
-    :param projection_coords: scan coordinates of projections.
-    :param obj_ref: ground truth image or reference image.
-    :param probe_ref: known or estimated complex probe function.
+    Function to perform WF/AWF reconstruction on ptychographic data.
+    :param dp: pre-processed diffraction pattern (intensity data).
+    :param project_coords: scan coordinates of projections.
+    :param init_obj: formulated initial guess of complex object.
+    :param init_probe: formulated initial guess of complex probe.
+    :param obj_ref: complex reference image.
+    :param probe_ref: complex reference image.
+    :param accel: with or without Nesterov acceleration.
     :param num_iter: number of iterations.
-    :param param: parameter is 1 by default.
-    :param param: with Nesterov Acceleration, AWF is performed.
-    :param cstr_win: pre-defined cover/window for comparing reconstruction results.
-    :param save_dir: save reconstruction results to the given directory.
-    :return: reconstructed complex transmittance of complex object and error metrics.
+    :param joint_recon: option to recover complex probe for blind ptychography.
+    :param cstr_win: pre-defined/assigned window for comparing reconstruction results.
+    :param save_dir: directory to save reconstruction results.
+    :return: reconstructed images and error metrics.
     """
-    # check directories
+    # check directory
     approach = 'AWF' if accel else 'WF'
-    if save_dir:
+    if save_dir is not None:
         os.makedirs(save_dir, exist_ok=True)
-    if cstr_win is None:
-        cstr_win = np.ones_like(init_obj_guess)
 
     # initialization
-    probe = np.copy(probe_ref)
-    img_norm = patch2img([np.abs(probe)**2] * len(diffract_data), projection_coords, init_obj_guess.shape)
-    obj_est = np.copy(init_obj_guess)
-    old_est = np.copy(init_obj_guess)
-    obj_nrmse_ls = []
-    diffr_nrmse_ls = []
-    time_ls = []
-    start_time = time.time()
-
-    # WF/AWF reconstruction
-    print('{} starts ...'.format(approach))
-    for i in range(num_iter):
-        if accel:
-            beta = (i + 2) / (i + 4)
-        else:
-            beta = 0
-        current_est = obj_est + beta * (obj_est - old_est)
-        old_est = np.copy(obj_est)
-        obj_est = wf_obj_func(current_est, probe, diffract_data, projection_coords, img_norm, param=param)
-
-        # compute time consumption
-        elapsed_time = time.time() - start_time
-        time_ls.append(elapsed_time)
-
-        # compute time consumption
-        elapsed_time = time.time() - start_time
-        time_ls.append(elapsed_time)
-
-        # compute the nrmse error between propagted reconstruction result and recorded measurements
-        diffraction_est = np.abs(compute_ft(probe*img2patch(np.copy(obj_est), projection_coords, diffract_data.shape)))
-        diffr_nrmse_val = compute_nrmse(diffraction_est, diffract_data, np.ones(diffract_data.shape))
-        diffr_nrmse_ls.append(diffr_nrmse_val)
-
-        # phase normalization and compute the nrmse error between reconstructed image and reference image
-        obj_revy = phase_norm(np.copy(obj_est) * cstr_win, np.copy(obj_ref) * cstr_win)
-        obj_nrmse_val = compute_nrmse(obj_revy * cstr_win, obj_ref * cstr_win, cstr_win)
-        obj_nrmse_ls.append(obj_nrmse_val)
-        print('iter =', i, 'img_error =', obj_nrmse_val, 'diffr_err =', diffr_nrmse_val)
-
-    # save recon results
-    save_tiff(obj_est, save_dir + 'obj_est_iter_{}.tiff'.format(i + 1))
-    save_array(obj_nrmse_ls, save_dir + 'obj_nrmse')
-    save_array(diffr_nrmse_ls, save_dir + 'diffr_nrmse')
-
-    # return results
-    keys = ['obj_revy', 'obj_err', 'diffr_err']
-    vals = [obj_revy, obj_nrmse_ls, diffr_nrmse_ls]
-    output = dict(zip(keys, vals))
-
-    return output
-
-
-def wf_joint_recon(init_obj, init_probe, diffraction_data, projection_coords, obj_ref=None, probe_ref=None,
-                   num_iter=100, obj_pm=1, probe_pm=1, accel=False, cstr_win=None, save_dir=None):
-    """
-    Function to perform Wirtinger Flow (WF) algorithm and Accelerated Wirtinger Flow (AWF) reconstruction.
-    :param init_obj: initialized complex object.
-    :param init_probe: initialized complex probe.
-    :param diffraction_data: pre-processed phaseless measurements / diffraction patterns.
-    :param projection_coords: coordinates of scan positions.
-    :param obj_ref: ground truth complex object.
-    :param probe_ref: ground truth complex probe.
-    :param num_iter: number of iterations.
-    :param obj_pm: scaler to adjust the step-size of object update function.
-    :param probe_pm: scaler to adjust the step-size of probe update function.
-    :param accel: with or without acceleration.
-    :param cstr_win: reconstruction window.
-    :param save_dir: path for saving reconstructed images.
-    :return: reconstructed complex image and probe and nrmse.
-    """
-    # check directories
-    approach = 'AWF' if accel else 'WF'
-    # check directories for saving files
-    if save_dir:
-        os.makedirs(save_dir, exist_ok=True)
     if cstr_win is None:
         cstr_win = np.ones_like(init_obj)
 
-    # initialization
-    obj_nrmse_ls = []
-    probe_nrmse_ls = []
-    diffr_nrmse_ls = []
-
     obj_est = np.copy(init_obj)
     obj_old_est = np.copy(init_obj)
+    probe_est = np.copy(init_probe) if joint_recon else np.copy(probe_ref)
+    probe_old_est = np.copy(init_probe) if joint_recon else np.copy(probe_ref)
 
-    probe_est = np.copy(init_probe)
-    probe_old_est = np.copy(init_probe)
+    obj_nrmse_ls = []
+    probe_nrmse_ls = []
+    dp_nrmse_ls = []
+    time_ls = []
 
-    start_time = time.time()
     # WF reconstruction
-    print('{} joint recon starts ...'.format(approach))
+    print('{} recon starts ...'.format(approach))
+    start_time = time.time()
     for i in range(num_iter):
         if accel:
             beta = (i + 2) / (i + 4)
@@ -176,37 +98,41 @@ def wf_joint_recon(init_obj, init_probe, diffraction_data, projection_coords, ob
             beta = 0
 
         # update estimate of complex object
-        # probe_mat = np.ones(diffraction_data.shape, dtype=np.complex128) * probe_est
-        probe_mat = [probe_est] * len(diffraction_data)
-        obj_step_sz_mat = patch2img(np.abs(probe_mat) ** 2, projection_coords, init_obj.shape)
+        probe_mat = [probe_est] * len(dp)
+        obj_step_sz_mat = patch2img(np.abs(probe_mat) ** 2, project_coords, init_obj.shape)
 
         current_obj_est = obj_est + beta * (obj_est - obj_old_est)
         obj_old_est = np.copy(obj_est)
-        obj_est = wf_obj_func(current_obj_est, probe_est, diffraction_data, projection_coords, obj_step_sz_mat, param=obj_pm)
+        obj_est = wf_obj_func(current_obj_est, probe_est, dp, project_coords, obj_step_sz_mat)
 
         # update estimate of complex probe
-        projected_img_patch = img2patch(obj_est, projection_coords, diffraction_data.shape)
-        probe_step_sz_mat = np.sum(np.abs(projected_img_patch) ** 2, 0)
+        if joint_recon:
+            projected_img_patch = img2patch(obj_est, project_coords, dp.shape)
+            probe_step_sz_mat = np.sum(np.abs(projected_img_patch) ** 2, 0)
 
-        current_probe_est = probe_est + beta * (probe_est - probe_old_est)
-        probe_old_est = np.copy(probe_est)
-        probe_mat_est = wf_probe_func(current_probe_est, projected_img_patch, diffraction_data, probe_step_sz_mat, param=probe_pm)
-        probe_est = np.average(probe_mat_est, axis=0)
+            current_probe_est = probe_est + beta * (probe_est - probe_old_est)
+            probe_old_est = np.copy(probe_est)
+            probe_mat_est = wf_probe_func(current_probe_est, projected_img_patch, dp, probe_step_sz_mat)
+            probe_est = np.average(probe_mat_est, axis=0)
 
-        # compute the nrmse error between propagted reconstruction result and recorded measurements
-        diffraction_est = np.abs(compute_ft(probe_est * img2patch(np.copy(obj_est), projection_coords, diffraction_data.shape)))
-        diffr_nrmse_val = compute_nrmse(diffraction_est, diffraction_data)
-        diffr_nrmse_ls.append(diffr_nrmse_val)
+        # calculate time consumption
+        elapsed_time = time.time() - start_time
+        time_ls.append(elapsed_time)
+
+        # compute the NRMSE between forward propagated reconstruction result and recorded measurements
+        dp_est = np.abs(compute_ft(probe_est * img2patch(np.copy(obj_est), project_coords, dp.shape)))
+        dp_nrmse_val = compute_nrmse(dp_est, dp)
+        dp_nrmse_ls.append(dp_nrmse_val)
 
         # phase normalization and scale image to minimize the intensity difference
-        if obj_ref:
+        if obj_ref is not None:
             obj_revy = phase_norm(np.copy(obj_est) * cstr_win, obj_ref * cstr_win)
-            img_nrmse_val = compute_nrmse(obj_revy * cstr_win, obj_ref * cstr_win, cstr_win)
-            obj_nrmse_ls.append(img_nrmse_val)
+            obj_nrmse_val = compute_nrmse(obj_revy * cstr_win, obj_ref * cstr_win, cstr_win)
+            obj_nrmse_ls.append(obj_nrmse_val)
         else:
             obj_revy = obj_est
 
-        if probe_ref:
+        if probe_ref is not None:
             probe_revy = phase_norm(np.copy(probe_est), probe_ref)
             probe_nrmse_val = compute_nrmse(probe_revy, probe_ref)
             probe_nrmse_ls.append(probe_nrmse_val)
@@ -215,18 +141,19 @@ def wf_joint_recon(init_obj, init_probe, diffraction_data, projection_coords, ob
 
     # calculate time consumption
     elapsed_time = time.time() - start_time
-    print('Time consumption of ePIE:', elapsed_time)
+    print('Time consumption of {}:'.format(approach), elapsed_time)
 
     # save recon results
     save_tiff(obj_est, save_dir + 'obj_est_iter_{}.tiff'.format(i + 1))
-    save_tiff(probe_est, save_dir + 'probe_est_iter_{}.tiff'.format(i + 1))
     save_array(obj_nrmse_ls, save_dir + 'obj_nrmse')
-    save_array(probe_nrmse_ls, save_dir + 'probe_nrmse')
-    save_array(diffr_nrmse_ls, save_dir + 'diffr_nrmse')
+    save_array(dp_nrmse_ls, save_dir + 'diffr_nrmse')
+    if joint_recon:
+        save_tiff(probe_est, save_dir + 'probe_est_iter_{}.tiff'.format(i + 1))
+        save_array(probe_nrmse_ls, save_dir + 'probe_nrmse')
 
-    # return the result
+    # return recon results
     keys = ['obj_revy', 'probe_revy', 'obj_err', 'probe_err', 'diffr_err']
-    vals = [obj_revy, probe_revy, obj_nrmse_ls, probe_nrmse_ls, diffr_nrmse_ls]
+    vals = [obj_revy, probe_revy, obj_nrmse_ls, probe_nrmse_ls, dp_nrmse_ls]
     output = dict(zip(keys, vals))
 
     return output
