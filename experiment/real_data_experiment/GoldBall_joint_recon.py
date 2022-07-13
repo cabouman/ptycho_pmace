1ed4ec8920f76496e9e74d74d0570bdce016cdaf
qiuchenzhai <qzhai@purdue.edu>
2022-07-13 10:57:38 -0600

    Update joint reconstruction experiment on GoldBalls dataset

--- experiment/real_data_experiment/GoldBall_joint_recon.py
+++ experiment/real_data_experiment/GoldBall_joint_recon.py
@@ -25,25 +25,11 @@ def build_parser():
     return parser
 
 
-def main():
-    # Arguments
-    parser = build_parser()
-    args = parser.parse_args()
-
-    # Load config file
-    with open(args.config_dir, 'r') as f:
-        config = yaml.safe_load(f)
-
-    # Read data from config file
-    data_dir = os.path.join(root_dir, config['data']['data_dir'])
-    display = config['data']['display']
-    out_dir = os.path.join(root_dir, config['output']['out_dir'])
-
-    # Load cxi file
-    f = h5py.File(data_dir, 'r')
+def read_cxi_data(fpath,display=False):
     
+    # load cxi file
+    f = h5py.File(fpath, 'r')
     with f:
-        # read the cxi file version
         cxi_version = np.array(f["cxi_version"])
         print('cxi_version = {}'.format(cxi_version))
 
@@ -77,26 +63,19 @@ def main():
         energy_J = np.array(f["entry_1/instrument_1/source_1/energy"])
         energy_eV = energy_J * 6.241509e18
         print('source energy:', energy_J, 'J', 'or equivalently', energy_eV, 'eV')
-    
-    source_wavelength = 1239.84193 / energy_eV
-    print('source wavelength:', source_wavelength, 'nm', 'or equivalently', source_wavelength *1e-9, 'm')
-    
-    # Adjust data
-    subtracted_data = data - np.average(dark_data, axis=0)
-    shifted_data = np.copy(subtracted_data)
-    shifted_data = np.roll(shifted_data, [1, 15], axis=(1, 2))
 
-    ## Preprocess data
-    #data[data < 0] = 0
-    data = (shifted_data + np.abs(shifted_data)) / 2.0
-    diffract_data = np.sqrt(data)
+    # compute source wavelength
+    source_wavelength = 1239.84193 / energy_eV
+    print('source wavelength:', source_wavelength, 'nm', 'or equivalently', source_wavelength * 1e-9, 'm')
+
+    # compute image piexel size
     img_pixel_sz = source_wavelength * 1e-9 * distance / (data.shape[1] * x_pixel_sz)
     print('image pixel size =', img_pixel_sz, 'm')
-        
-    # convert units of translations from m to pixels
-    reversed_trans = np.copy(trans)
-    reversed_trans[:, 1] = -trans[:, 1]
-    trans_px = reversed_trans / img_pixel_sz + data.shape[1] / 2 + 12
+
+    # convert translations from units of meters to units of pixels
+    shifted_trans = np.copy(trans)
+    shifted_trans[: ,1] += np.abs(np.amin(trans[:, 1]))
+    trans_px = shifted_trans / img_pixel_sz + data.shape[1] / 2 + 12
     if display:
         plt.subplot(121)
         plt.plot(trans[:, 0], trans[:, 1], 'r.')
@@ -112,6 +91,65 @@ def main():
         plt.title('translation (in pixels)')
         plt.show()
         plt.clf()
+   
+    # subtract dark data
+    avg_dark_data = np.average(dark_data, axis=0)
+    subtracted_data = data - avg_dark_data
+    if display:
+        plt.subplot(221)
+        plt.imshow(dark_data[0], cmap='gray', vmax=400)
+        plt.colorbar()
+        plt.axis('off')
+        plt.title('dark data')
+        plt.subplot(222)
+        plt.imshow(avg_dark_data, cmap='gray', vmax=400)
+        plt.colorbar()
+        plt.axis('off')
+        plt.title('average of dark data')
+        plt.subplot(223)
+        plt.imshow(data[0], cmap='gray', vmax=6400)
+        plt.colorbar()
+        plt.axis('off')
+        plt.title('diffraction pattern')
+        plt.subplot(224)
+        plt.imshow(subtracted_data[0], cmap='gray', vmax=6400)
+        plt.colorbar()
+        plt.axis('off')
+        plt.title('subtracted data')
+        plt.show()
+        plt.clf()
+
+    # center each diffraction pattern
+    shifted_data = np.copy(subtracted_data)
+    shifted_data = np.roll(shifted_data, [1, 15], axis=(1, 2))
+    if display: 
+        plt.subplot(121)
+        plt.imshow(subtracted_data[0], cmap='gray', vmax=400)
+        plt.colorbar()
+        plt.title('subtracted data')
+        plt.subplot(122)
+        plt.imshow(shifted_data[0], cmap='gray', vmax=400)
+        plt.colorbar()
+        plt.title('shifted data')
+        plt.show()
+        plt.clf()
+
+    return shifted_data, trans_px
+
+
+def main():
+    # arguments
+    parser = build_parser()
+    args = parser.parse_args()
+
+    # load config file
+    with open(args.config_dir, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # read data from config file
+    data_dir = os.path.join(root_dir, config['data']['data_dir'])
+    display = config['data']['display']
+    out_dir = os.path.join(root_dir, config['output']['out_dir'])
 
     # Determine time stamp
     today_date = dt.date.today()
@@ -127,6 +165,16 @@ def main():
     except OSError as error:
         print("Output directory '%s' can not be created" % save_dir)
 
+    # load data from file
+    y_dir = os.path.join(data_dir, 'frame_data/')
+    if os.path.isfile(y_dir):
+        y_meas = load_measurement(y_dir, display=False)
+    trans_dir = os.path.join(data_dir, 'Translation.tsv.txt')
+    if os.path.isfile(trans_dir):
+        trans_px = pd.read_csv(trans_dir, sep=None, engine='python', header=0)
+    else:
+        print('Error. Call function read_cxi_data().')
+
     # Default parameters
     rand_seed = 0
     np.random.seed(rand_seed)
@@ -137,7 +185,6 @@ def main():
     # Generate formulated initial guess for reconstruction
     init_obj = np.ones((750, 750), dtype=np.complex128)
     init_probe = gen_init_probe(diffract_data, projection_coords, obj_ref=init_obj, display=True)
-    init_obj = gen_init_obj(diffract_data, projection_coords, obj_ref=init_obj, probe_ref=init_probe, display=True)
     
     # reconstruction arguments
     args = dict(init_obj=init_obj, init_probe=init_probe, 
@@ -147,7 +194,13 @@ def main():
     obj_ss = config['ePIE']['obj_step_sz']
     probe_ss = config['ePIE']['probe_step_sz']
     epie_dir = save_dir + 'ePIE/obj_ss_{}_probe_ss_{}/'.format(obj_ss, probe_ss)
-    epie_result = pie.epie_recon(diffract_data, projection_coords, obj_step_sz=obj_ss, probe_step_sz=probe_ss, save_dir=epie_dir, **args)
+    epie_result = pie.epie_recon(y_meas, projection_coords, obj_step_sz=obj_ss, probe_step_sz=probe_ss, save_dir=epie_dir, **args)
+
+    # PMACE recon
+    obj_alpha = config['PMACE']['obj_alpha']
+    probe_alpha = config['PMACE']['probe_alpha']
+    pmace_dir = save_dir + 'PMACE/obj_alpha_{}_probe_alpha_{}/'.format(obj_alpha, probe_alpha)
+    pmace_result = pmace.pmace_recon(y_meas, projection_coords, obj_pm=obj_alpha, probe_pm=probe_alpha, save_dir=pmace_dir)
 
 
 #def plot_GoldBall_img(cmplx_img, img_title, display_win=None, display=False, save_dir=None):
