b0da248fc03cf6323fc73966caf56787dff0c763
qiuchenzhai <qzhai@purdue.edu>
2022-07-14 14:28:14 -0600

    Update demo_pmace

--- demo/demo_pmace.py
+++ demo/demo_pmace.py
@@ -2,6 +2,8 @@ import sys
 from pathlib import Path
 root_dir = Path(__file__).parent.absolute().parent.absolute()
 sys.path.append(str(root_dir))
+root_root_dir = Path(__file__).parent.absolute().parent.absolute().parent.absolute()
+sys.path.append(str(root_root_dir))
 import os, argparse, yaml
 import datetime as dt
 from shutil import copyfile
@@ -50,76 +52,75 @@ def main():
     np.random.seed(rand_seed)
 
     # Load ground truth images from file
-    obj_ref = load_img(obj_dir)
-    probe_ref = load_img(probe_dir)
+    ref_obj = load_img(obj_dir)
+    ref_probe = load_img(probe_dir)
 
     # Load intensity only measurements(data) from file and pre-process the data
-    diffraction_data = load_measurement(data_dir + 'frame_data/', display=display)
+    y_meas = load_measurement(data_dir + 'frame_data/', display=display)
 
     # Load scan points
-    scan_loc_data = pd.read_csv(data_dir + 'Translations.tsv.txt', sep=None, engine='python', header=0)
-    scan_loc = scan_loc_data[['FCx', 'FCy']].to_numpy()
+    scan_file = pd.read_csv(data_dir + 'Translations.tsv.txt', sep=None, engine='python', header=0)
+    scan_loc = scan_file[['FCx', 'FCy']].to_numpy()
 
     # calculate the coordinates of projections
-    projection_coords = get_proj_coords_from_data(scan_loc, diffraction_data)
+    patch_bounds = get_proj_coords_from_data(scan_loc, y_meas)
 
     # Generate formulated initial guess for reconstruction
-    init_obj = gen_init_obj(diffraction_data, projection_coords, obj_ref=obj_ref, probe_ref=probe_ref, display=display)
+    init_obj = gen_init_obj(y_meas, patch_bounds, obj_ref=ref_obj, probe_ref=ref_probe, display=display)
 
     # Produce the cover/window for comparison
     if window_coords is not None:
         xmin, xmax, ymin, ymax = window_coords[0], window_coords[1], window_coords[2], window_coords[3]
-        display_win = np.zeros(init_obj.shape)
-        display_win[xmin:xmax, ymin:ymax] = 1
+        recon_win = np.zeros(init_obj.shape)
+        recon_win[xmin:xmax, ymin:ymax] = 1
     else:
-        display_win = None
+        recon_win = None
 
     # Reconstruction parameters and arguments
-    recon_args = dict(init_obj=init_obj, obj_ref=obj_ref, probe_ref=probe_ref, 
-                      num_iter=config['recon']['num_iter'], joint_recon=False, cstr_win=display_win)
-    fig_args = dict(ref_img=obj_ref, display_win=display_win, display=display)
+    recon_args = dict(init_obj=init_obj, ref_obj=ref_obj, ref_probe=ref_probe, recon_win=recon_win,num_iter=config['recon']['num_iter'], joint_recon=False)
+    fig_args = dict(ref_img=ref_obj, display_win=recon_win, display=display)
 
     # ePIE recon
     step_sz = config['ePIE']['step_sz']
     epie_dir = save_dir + config['ePIE']['out_dir']
-    epie_result = pie.epie_recon(diffraction_data, projection_coords, obj_step_sz=step_sz, save_dir=epie_dir, **recon_args)
+    epie_result = pie.epie_recon(y_meas, patch_bounds, obj_step_sz=step_sz, save_dir=epie_dir, **recon_args)
     # Plot reconstructed image
-    plot_synthetic_img(epie_result['obj_revy'], img_title='ePIE', save_dir=epie_dir, **fig_args)
+    plot_synthetic_img(epie_result['object'], img_title='ePIE', save_dir=epie_dir, **fig_args)
 
     # Wirtinger Flow (WF) recon
     wf_dir = save_dir + config['WF']['out_dir']
-    wf_result = wf.wf_recon(diffraction_data, projection_coords, accel=False, save_dir=wf_dir, **recon_args)
-    plot_synthetic_img(wf_result['obj_revy'], img_title='WF', save_dir=wf_dir, **fig_args)
+    wf_result = wf.wf_recon(y_meas, patch_bounds, accel=False, save_dir=wf_dir, **recon_args)
+    plot_synthetic_img(wf_result['object'], img_title='WF', save_dir=wf_dir, **fig_args)
 
     # Acclerated Wirtinger Flow (AWF) recon
     awf_dir = save_dir + config['AWF']['out_dir']
-    awf_result = wf.wf_recon(diffraction_data, projection_coords, accel=True, save_dir=awf_dir, **recon_args)
-    plot_synthetic_img(awf_result['obj_revy'], img_title='AWF', save_dir=awf_dir, **fig_args)
+    awf_result = wf.wf_recon(y_meas, patch_bounds, accel=True, save_dir=awf_dir, **recon_args)
+    plot_synthetic_img(awf_result['object'], img_title='AWF', save_dir=awf_dir, **fig_args)
 
     # SHARP recon
     relax_pm = config['SHARP']['relax_pm']
     sharp_dir = save_dir + config['SHARP']['out_dir']
-    sharp_result = sharp.sharp_recon(diffraction_data, projection_coords, relax_pm=relax_pm, save_dir=sharp_dir, **recon_args)
-    plot_synthetic_img(sharp_result['obj_revy'], img_title='SHARP', save_dir=sharp_dir, **fig_args)
+    sharp_result = sharp.sharp_recon(y_meas, patch_bounds, relax_pm=relax_pm, save_dir=sharp_dir, **recon_args)
+    plot_synthetic_img(sharp_result['object'], img_title='SHARP', save_dir=sharp_dir, **fig_args)
 
     # SHARP+ recon
     relax_pm = config['SHARP_plus']['relax_pm']
     sharp_plus_dir = save_dir + config['SHARP_plus']['out_dir']
-    sharp_plus_result = sharp.sharp_plus_recon(diffraction_data, projection_coords, relax_pm=relax_pm, save_dir=sharp_plus_dir, **recon_args)
-    plot_synthetic_img(sharp_plus_result['obj_revy'], img_title='SHARP+', save_dir=sharp_plus_dir, **fig_args)
+    sharp_plus_result = sharp.sharp_plus_recon(y_meas, patch_bounds, relax_pm=relax_pm, save_dir=sharp_plus_dir, **recon_args)
+    plot_synthetic_img(sharp_plus_result['object'], img_title='SHARP+', save_dir=sharp_plus_dir, **fig_args)
 
     # PMACE recon
     alpha = config['PMACE']['alpha']                   
     pmace_dir = save_dir + config['PMACE']['out_dir']
-    pmace_result = pmace.pmace_recon(diffraction_data, projection_coords, obj_pm=alpha, add_reg=False, save_dir=pmace_dir, **recon_args)
-    plot_synthetic_img(pmace_result['obj_revy'], img_title='PMACE', save_dir=pmace_dir, **fig_args)
+    pmace_result = pmace.pmace_recon(y_meas, patch_bounds, obj_data_fit_prm=alpha, add_reg=False, save_dir=pmace_dir, **recon_args)
+    plot_synthetic_img(pmace_result['object'], img_title='PMACE', save_dir=pmace_dir, **fig_args)
 
     # reg-PMACE recon
     alpha = config['reg-PMACE']['alpha']
     bm3d_psd = config['reg-PMACE']['bm3d_psd']      
     reg_pmace_dir = save_dir + config['reg-PMACE']['out_dir']
-    reg_pmace_result = pmace.pmace_recon(diffraction_data, projection_coords, obj_pm=alpha, add_reg=True, sigma=bm3d_psd, save_dir=reg_pmace_dir, **recon_args)
-    plot_synthetic_img(reg_pmace_result['obj_revy'], img_title='reg-PMACE', save_dir=reg_pmace_dir, **fig_args)
+    reg_pmace_result = pmace.pmace_recon(y_meas, patch_bounds, obj_data_fit_prm=alpha, add_reg=True, sigma=bm3d_psd, save_dir=reg_pmace_dir, **recon_args)
+    plot_synthetic_img(reg_pmace_result['object'], img_title='reg-PMACE', save_dir=reg_pmace_dir, **fig_args)
 
     # Save config file to output directory
     if not os.path.exists(save_dir):
@@ -129,9 +130,9 @@ def main():
     # Convergence plots
     xlabel, ylabel = 'Number of iteration', 'NRMSE value in log scale'
     line_label = 'nrmse'
-    nrmse = {'ePIE': epie_result['obj_err'], 'WF': wf_result['obj_err'], 'AWF': awf_result['obj_err'],
-             'SHARP': sharp_result['obj_err'], 'SHARP+': sharp_plus_result['obj_err'],
-             'PMACE': pmace_result['obj_err'], 'reg-PMACE': reg_pmace_result['obj_err']}
+    nrmse = {'ePIE': epie_result['err_obj'], 'WF': wf_result['err_obj'], 'AWF': awf_result['err_obj'],
+             'SHARP': sharp_result['err_obj'], 'SHARP+': sharp_plus_result['err_obj'],
+             'PMACE': pmace_result['err_obj'], 'reg-PMACE': reg_pmace_result['err_obj']}
     plot_nrmse(nrmse, title='Convergence plots of PMACE', label=[xlabel, ylabel, line_label],
                step_sz=10, fig_sz=[8, 4.8], display=display, save_fname=save_dir + 'convergence_plot')
 
