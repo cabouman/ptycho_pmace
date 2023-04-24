3ee86b4cf38ccfc1eb1bde3d6a5d855e68585c70
qiuchenzhai <qzhai@purdue.edu>
2023-04-24 10:51:10 -0400

    Convert to a decorator to prepare function

--- ptycho/oo_pmace.py
+++ ptycho/oo_pmace.py
@@ -3,14 +3,23 @@ from ptycho.pmace import pmace_recon
 
 
 class PMACE():
+    """This class is a decorator that can be used to prepare a function before it is called.
+
+    Args:
+        func (function): The function to be decorated.
+        *args: Positional arguments to be passed to the decorated function.
+        **kwargs: Keyword arguments to be passed to the decorated function.
+    """
     def __init__(self, func, *args, **kwargs):
         self.func = func
         self.args = args
-        self.kwargs = kwargs        
+        self.kwargs = kwargs   
+        
     def __call__(self):
         def wrapper(*args, **kwargs):
             print('Preparing function ...')
             return_val = self.func(*args, **kwargs)
             return return_val
+        
         return wrapper(*self.args, **self.kwargs)
     
