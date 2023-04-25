7dc34b80ffcf430da46b3d7f6bd429c17c9ee706
qiuchenzhai <qzhai@purdue.edu>
2023-04-25 15:26:00 -0400

    Reorganize PMACE functions

--- ptycho/pmace.py
+++ ptycho/pmace.py
@@ -1,9 +1,31 @@
-import time
+import time, os
 from utils.utils import *
 from utils.nrmse import *
 from bm4d import bm4d, BM4DProfile, BM4DStages, BM4DProfile2D, BM4DProfileComplex, BM4DProfileBM3DComplex
 
 
+class PMACE():
+    """This class is a decorator that can be used to prepare a function before it is called.
+
+    Args:
+        func (function): The function to be decorated.
+        *args: Positional arguments to be passed to the decorated function.
+        **kwargs: Keyword arguments to be passed to the decorated function.
+    """
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs   
+        
+    def __call__(self):
+        def wrapper(*args, **kwargs):
+            print('Preparing function ...')
+            return_val = self.func(*args, **kwargs)
+            return return_val
+        
+        return wrapper(*self.args, **self.kwargs)
+    
+    
 def prox_map_op(cur_est, joint_est, y_meas, data_fit_prm):
     r"""Data-fitting operator.
 
