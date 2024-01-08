a97b27acedb2133831cab43f3462cf9a33f9089e
Qiuchen Zhai <qzhai@b000.negishi.rcac.purdue.edu>
2024-01-08 12:55:48 -0500

    Revise comments and documentation

--- demo/demo_pmace.py
+++ demo/demo_pmace.py
@@ -1,8 +1,11 @@
-import demo_utils
-import argparse, yaml
-import time
+import argparse
+import os
+import yaml
+import pandas as pd
+import numpy as np
 from pmace.utils import *
 from pmace.pmace import *
+import demo_utils
 
 
 '''
@@ -22,86 +25,74 @@ print('This script demonstrates reconstruction of complex transmittance image us
 
 
 def build_parser():
-    parser = argparse.ArgumentParser(description='Ptychographic image reconstruction using PMACE.')
-    parser.add_argument('config_dir', type=str, help='Path to config file.', nargs='?', const='demo_pmace.yaml',
+    parser = argparse.ArgumentParser(description='PMACE demo for ptychographic image reconstruction.')
+    parser.add_argument('config_dir', type=str, help='Path to config file.', nargs='?', 
+                        const='demo_pmace.yaml',
                         default='config/demo_pmace.yaml')
     return parser
 
 
 def main():
-    # load config file and pass arguments
+    # Load config file and parse arguments
     parser = build_parser()
     args = parser.parse_args()
     print("Passing arguments ...")
     with open(args.config_dir, 'r') as f:
         config = yaml.safe_load(f)
 
-    # url to dataset.
+    # URLs and paths
     dataset_url = config['dataset']['download_url']
-    # destination path to download and extract the phantom and NN weight files.
-    dataset_dir = config['dataset']['save_dir']  
-    # path to reference object, probe and intensity measurements
-    obj_dir = dataset_dir + config['data']['obj_dir']
-    probe_dir = dataset_dir + config['data']['probe_dir']
-    data_dir = dataset_dir + config['data']['data_dir']
+    dataset_dir = config['dataset']['save_dir']
+    obj_dir = os.path.join(dataset_dir, config['data']['obj_dir'])
+    probe_dir = os.path.join(dataset_dir, config['data']['probe_dir'])
+    data_dir = os.path.join(dataset_dir, config['data']['data_dir'])
     window_coords = config['data']['window_coords']
     save_dir = config['recon']['out_dir']
     
-    # download dataset. 
+    # Download dataset
     dataset_path = download_and_extract(dataset_url, dataset_dir)
 
-    # check directory
+    # Check and create the output directory
     print("Creating output directory '%s' ..." % save_dir)
-    try:
-        os.makedirs(save_dir, exist_ok=True)
-    except OSError as error:
-        print("Output directory can not be created")
+    os.makedirs(save_dir, exist_ok=True)
 
-    # load reference images from file
+    # Load reference images
     print("Loading data ...")
     ref_obj = load_img(obj_dir)
     ref_probe = load_img(probe_dir)
 
-    # load measurements (diffraction patterns) from file and pre-process data
-    y_meas = load_measurement(data_dir + 'frame_data/')
+    # Load measurements (diffraction patterns) and pre-process data
+    y_meas = load_measurement(os.path.join(data_dir, 'frame_data/'))
 
-    # load scan positions
-    scan_loc_file = pd.read_csv(data_dir + 'Translations.tsv.txt', sep=None, engine='python', header=0)
+    # Load scan positions
+    scan_loc_file = pd.read_csv(os.path.join(data_dir, 'Translations.tsv.txt'), sep=None, engine='python', header=0)
     scan_loc = scan_loc_file[['FCx', 'FCy']].to_numpy()
 
-    # calculate coordinates of projections from scan positions
+    # Calculate coordinates of projections from scan positions
     patch_crds = get_proj_coords_from_data(scan_loc, y_meas)
 
-    # formulate initial guess of complx object for reconstruction
+    # Formulate initial guess of complex object for reconstruction
     init_obj = gen_init_obj(y_meas, patch_crds, ref_obj.shape, ref_probe=ref_probe)
 
-    # pre-define reconstruction region
+    # Pre-define reconstruction region
     if window_coords is not None:
-        xmin, xmax, ymin, ymax = window_coords[0], window_coords[1], window_coords[2], window_coords[3]
+        xmin, xmax, ymin, ymax = window_coords
     else:
-        xmin, xmax, ymin, ymax = np.amin(scan_loc[0]), np.amax(scan_loc[0]), np.amin(scan_loc[1]), np.amax(scan_loc[1])
+        xmin, xmax, ymin, ymax = np.amin(scan_loc[:, 0]), np.amax(scan_loc[:, 0]), np.amin(scan_loc[:, 1]), np.amax(scan_loc[:, 1])
     recon_win = np.zeros(init_obj.shape)
     recon_win[xmin:xmax, ymin:ymax] = 1
 
-    # reconstruction parameters
+    # Reconstruction parameters
     num_iter = config['recon']['num_iter']
     joint_recon = config['recon']['joint_recon']
     alpha = config['recon']['data_fit_param']
     fig_args = dict(display_win=recon_win, save_dir=save_dir)
-                    
-    # PMACE recon
-    pmace_obj = PMACE(pmace_recon, y_meas, patch_crds, init_obj, ref_obj=ref_obj, ref_probe=ref_probe, 
+    
+    # PMACE reconstruction
+    pmace_obj = PMACE(pmace_recon, y_meas, patch_crds, init_obj, ref_obj=ref_obj, ref_probe=ref_probe,
                       num_iter=num_iter, obj_data_fit_prm=alpha, recon_win=recon_win, save_dir=save_dir)
     pmace_result = pmace_obj()
     demo_utils.plot_synthetic_img(pmace_result['object'], img_title='PMACE', **fig_args)
-    
-    # # recon with regularization
-    # reg_pmace_obj = PMACE(pmace_recon, y_meas, patch_crds, init_obj, ref_obj=ref_obj, ref_probe=ref_probe, 
-    #                       recon_win=recon_win, save_dir=save_dir, num_iter=num_iter, 
-    #                       obj_data_fit_prm=alpha, add_reg=True, sigma=sigma)
-    # reg_pmace_result = reg_pmace_obj()
-    # demo_utils.plot_synthetic_img(reg_pmace_result['object'], img_title='reg-PMACE', **fig_args)
-    
-    
+
 if __name__ == '__main__':
     main()
--- demo/demo_utils.py
+++ demo/demo_utils.py
@@ -1,25 +1,38 @@
-import sys, os
+import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-from pmace.display import *
+from pmace.display import plot_cmplx_img
 
 
 def plot_synthetic_img(cmplx_img, img_title, display_win=None, save_dir=None):
-    """Display and save demo result.
-    
+    """
+    Display and save a demo result.
+
     Args:
         cmplx_img: complex image array.
-        img_title: title of plot image.
-        display_win: window to plot image.
-        save_dir: directory for saving image.
+        img_title: title of the plot image.
+        display_win: window to plot the image.
+        save_dir: directory for saving the image.
+    
+    Example:
+    plot_synthetic_img(cmplx_img, img_title="demo_img", display_win=None, save_dir='/path/to/save/directory/')
     """
     if save_dir is not None:
         os.makedirs(save_dir, exist_ok=True)
-    save_fname = None if (save_dir is None) else save_dir + '{}_recon_cmplx_img'.format(img_title)
+        
+    save_fname = None if (save_dir is None) else os.path.join(save_dir, f'{img_title}_recon_cmplx_img')
 
-    # plot complex image
-    plot_cmplx_img(cmplx_img, img_title=img_title, display_win=display_win, save_fname=save_fname,
-                   mag_vmax=1, mag_vmin=0.5, phase_vmax=0, phase_vmin=-np.pi/4,
-                   real_vmax=1.1, real_vmin=0.8, imag_vmax=0, imag_vmin=-0.6)
-    
-    
+    # Plot complex image with customized parameters
+    plot_cmplx_img(cmplx_img, 
+                   img_title=img_title, 
+                   display_win=display_win, 
+                   save_fname=save_fname,
+                   mag_vmax=1, 
+                   mag_vmin=0.5, 
+                   phase_vmax=0, 
+                   phase_vmin=-np.pi/4,
+                   real_vmax=1.1, 
+                   real_vmin=0.8, 
+                   imag_vmax=0, 
+                   imag_vmin=-0.6)
